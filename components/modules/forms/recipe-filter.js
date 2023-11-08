e6f3a4d641d968cbf1f40d6a58a149d2bf969ac7
jcock <jason.cockerham@gmail.com>
2023-11-08 09:04:01 -0500

    Removed form

--- components/modules/forms/recipe-filter.js
+++ components/modules/forms/recipe-filter.js
@@ -1,38 +1,18 @@
 import { forwardRef } from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
 import * as Select from '@radix-ui/react-select';
 import { twMerge } from 'tailwind-merge';
 
 import { inter } from '~/styles/fonts';
 
-import Badge from '~/components/modules/badge';
 import Icon from '~/components/modules/icon';
 
 import RecipeTypes from 'data/recipe-types';
-
-let validationSchema = Yup.object().shape({
-	recipes: Yup.string()
-		.required('(select a life stage)')
-});
 
 const FormRecipeFilter = ({
 	setFilter,
 	filteredValue,
 	className
 }) => {
-	const {
-		register,
-		formState:{ errors }
-	} = useForm({
-		mode: 'onBlur',
-		defaultValues: {
-			recipes: 'all'
-		},
-		resolver: yupResolver(validationSchema)
-	});
-
 	const updateFilter = item => {
 		setFilter(item);
 	};
